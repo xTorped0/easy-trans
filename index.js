7d19f8eac3fd7062985c50767cc893ec98a06cfd
Torped0 <dabizha.vanya@gmail.com>
2023-03-19 14:20:56 +0200

    some additional changes

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-let map = L.map('map', { scrollWheelZoom: false, center: [50.4101, 30.5234], zoom: 9 });
+let map = L.map('map', { scrollWheelZoom: false, center: [50.4101, 30.5234], zoom: 6.4 });
 
 const tile = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
 L.geoJSON(poly.geometries, {
 	weight: 1,
 	fillOpacity: 0.3,
-	color: 'blue',
+	color: '#fead15',
 	dashArray: '3'
 }).addTo(map);
 
+L.geoJSON(polyobl.geometries, {
+	weight: 2,
+	fillOpacity: 0,
+	color: '#fead15',
+	dashArray: '3'
+}).addTo(map);
